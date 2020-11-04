4b4c1c56bac0478e752956c7b3356f06123d0169
Quit3Simpl3 <abramovichavishag@gmail.com>
2020-11-04 12:10:36 +0200

    commented out hello world

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(int argc, char** argv){
-    cout << "Hello world!" << endl;
+//    cout << "Hello world!" << endl;
     if(argc != 2){
         cout << "usage cTrace <config_path>" << endl;
         return 0;
