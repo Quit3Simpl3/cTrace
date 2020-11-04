5006dc7af4eeff44024e3070b47c875cc0a26706
Quit3Simpl3 <abramovichavishag@gmail.com>
2020-11-04 12:09:03 +0200

    commented out session

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Session.h"
+//#include "Session.h"
 
 using namespace std;
 
@@ -9,7 +9,7 @@ int main(int argc, char** argv){
         cout << "usage cTrace <config_path>" << endl;
         return 0;
     }
-    Session sess(argv[1]);
-    sess.simulate();
+    /*Session sess(argv[1]);
+    sess.simulate();*/
     return 0;
 }
