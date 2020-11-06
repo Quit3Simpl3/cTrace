25db535045411195d26373eedf505b1d116164fa
Quit3Simpl3 <abramovichavishag@gmail.com>
2020-11-06 15:30:01 +0200

    added new brach for json parsing

--- Session.cpp
+++ Session.cpp
@@ -1,5 +1,6 @@
 #include "Session.h"
 #include "json.hpp"
+#include "Graph.h"
 #include <fstream>
 
 using namespace std;
@@ -9,38 +10,5 @@ Session::Session(const std::string &path) {
     fstream stream(path);
     json j;
     stream >> j;
-    vector<vector<int>> matrix;
-    json j_graph = j.at("graph");
-    for (int i = 0; i < j_graph.size(); ++i) {
-        vector<int> v = j_graph.at(i); // TODO: move/copy-constructor???
-        matrix.push_back(v);
-    }
-    this->setGraph(new Graph(json.parse_to_vector("path")["graph"]));
-
-    this->treeType = ;
-    this->agents = ;
-}
-
-void Session::simulate() {
-
-}
-
-void Session::addAgent(const Agent &agent) {
-
-}
-
-void Session::setGraph(const Graph &graph) {
-    this->g = graph;
-}
-
-void Session::enqueueInfected(int) {
-
-}
-
-int Session::dequeueInfected() {
-    return 0;
-}
-
-TreeType Session::getTreeType() const {
-    return Root;
+    vector<vector<int>> matrix = j.at("graph"); // creates adjacency matrix from config JSON
 }
